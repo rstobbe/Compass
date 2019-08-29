93c26d4d62c824f690c5652ef3da52e49c7a5fa6
apu031 <apu.islam@ualberta.ca>
2019-08-29 09:04:27 -0600

    dev

--- 1 Scripts/zs Shared/2 Image Construction/Convolution/Conv_Samp2Grid/S2GCUDADoubleC_v5b/mS2GCUDADoubleC_v5b.m
+++ 1 Scripts/zs Shared/2 Image Construction/Convolution/Conv_Samp2Grid/S2GCUDADoubleC_v5b/mS2GCUDADoubleC_v5b.m
@@ -24,12 +24,21 @@ end
 Status2('busy','CUDA',StatLev);
 
 %------------------------------------
-% CUDA Specifics
+% Function Hardcoded for 2 GPUs
 %------------------------------------
 NumberGpus = CUDA.Index;
+if NumberGpus > 2
+    NumberGpus = 2;
+end
+
+%------------------------------------
+% CUDA Specifics
+%------------------------------------
 ComputeCapability = str2double(CUDA.ComputeCapability);
-if ComputeCapability == 6.1 || ComputeCapability == 6.2
+if ComputeCapability == 6.1 || ComputeCapability == 6.2 
     CoresPerMultiProcessor = 128;
+elseif ComputeCapability == 7.5
+    CoresPerMultiProcessor = 64;       % Number of ALU lanes for integer and single-precision floating-point arithmetic operations	
 end
 MultiprocessorCount = CUDA.MultiprocessorCount;
 TotalCoresInPlay = CoresPerMultiProcessor*MultiprocessorCount*NumberGpus;
